ebf14de1af221e477568d2b8058784bbcf8e00ed
jettaponB <chettapon186@gmail.com>
2021-08-22 15:40:38 +0700

    new

---
+++ Test08.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+def set_message():
+    text = text_input.get()
+    title_label.configure(text=text)
+
+window = tk.Tk()
+window.title('Desktop Application')
+window.minsize(width=400, height=600)
+
+title_label = tk.Label(master=window, text='กรุณาระบุข้อความ')
+title_label.pack()
+
+text_input = tk.Entry(master=window)
+text_input.pack()
+
+ok_button = tk.Button(master=window, text='OK', command=set_message)
+ok_button.pack()
+
+window.mainloop()
+
+
+
---
+++ test09.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+
+def show_output():
+    number = int(input_number.get())
+
+    output = ''
+    for i in range(1, 13):
+        output += str(number) + ' * ' + str(i) + ' = ' + str(number * i) + '\n'
+        output_label.configure(text=output)
+
+
+window = tk.Tk()
+window.title('โปรแกรมคำนวนสูตรคูณ')
+window.minsize(width=400, height=400)
+
+title_label = tk.Label(master=window, text='กรุณาระบุแม่สูตรคูณ')
+title_label.pack()
+
+input_number = tk.Entry(master=window)
+input_number.pack()
+
+cal_button = tk.Button(master=window, text='คำนวน', command=show_output)
+cal_button.pack()
+
+output_label = tk.Label(master=window)
+output_label.pack()
+
+window.mainloop()

e5a1451d098ecfdddac5571da5d414a6c7e2274c
jettaponB <chettapon186@gmail.com>
2021-08-22 13:55:36 +0700

    new

---
+++ Test01.py
@@ -0,0 +1,27 @@
+# x = '4.5'
+# y = str(12)
+# z = x + y
+# print(z)
+
+# final_score = 15
+#
+# age = 25                # ตัวเลขจำนวนเต็ม (integer)
+# weight = 66.6           # ตัวเลขทศนิยม (Float)
+# first_name = 'ศักรินทร์'   # ข้อความ (String)
+# has_notebook = True     # Boolean
+
+x = 5
+y = 2
+
+a1 = x + y      # 7
+a2 = x - y      # 3
+a3 = x * y      # 10
+a4 = x / y      # 2.5
+a5 = x % y      # 1
+a6 = x ** y     # 25
+a7 = x // y     # 2
+a8 = (x + 1) * (y - 1)
+
+x = x + 3       # x += 3
+
+print(a8)
